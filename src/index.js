50def21893b2aa22f1e5d5c59b802e80d1fb5ab6
ha quang le <winlp4ever@gmail.com>
2020-07-04 17:16:20 +0200

    update design

--- src/index.js
+++ src/index.js
@@ -3,20 +3,6 @@ import ReactDOM from "react-dom";
 import "./_common.scss";
 import App from './app/app';
 import $ from 'jquery';
-import { initializeIcons } from '@uifabric/icons';
-initializeIcons();
-
-var lastScrollTop = $(window).scrollTop();
-
-$(window).on({
-    scroll: (e) => {
-        let n = $(window).scrollTop();
-        if (n < 4) $('.menu').attr('class', 'menu init');
-        else $('.menu').attr('class', 'menu');
-        lastScrollTop = n; 
-    }
-})
-
 
 function renderWeb() {
     ReactDOM.render(<App />, document.getElementById('main'));
