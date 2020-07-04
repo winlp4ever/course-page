b03b856d28d8b4537fb2366012fa63e6df7f9a58
ha quang le <winlp4ever@gmail.com>
2020-07-04 17:21:30 +0200

    update pkgs

--- src/index.js
+++ src/index.js
@@ -1,24 +1,25 @@
-import React, {Component} from "react";
-import ReactDOM from "react-dom";
-import "./_common.scss";
-import App from './app/app';
-import $ from 'jquery';
+import React, {Component} from "react"
+import ReactDOM from "react-dom"
+import "./_common.scss"
+import "./_variables.scss"
+import App from './app/app'
+import $ from 'jquery'
 
 function renderWeb() {
-    ReactDOM.render(<App />, document.getElementById('main'));
+    ReactDOM.render(<App />, document.getElementById('main'))
 }
-renderWeb();
+renderWeb()
 
 
 if (module.hot) {
-    console.log('what fuct');
+    console.log('what fuct')
     module.hot.accept(
         [
             './app/app', 
         ], () => {
-            console.log('what merde');
-            renderWeb();
+            console.log('what merde')
+            renderWeb()
         }
-    );
-    module.hot.accept();
+    )
+    module.hot.accept()
 }
